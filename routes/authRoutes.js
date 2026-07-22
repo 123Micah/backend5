03248777d86828cb5bfd878e1e02d98777a0f1ab
123micah <luckyokperuvwe@gmail.com>
2026-07-22 12:23:51 +0100

    Add admin register route (temporary)

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,11 +4,13 @@ const router = express.Router();
 
 const {
   adminLogin,
+  adminRegister,
   studentLogin,
   studentRegister,
 } = require('../controllers/authController');
 
 router.post('/admin/login', adminLogin);
+router.post('/admin/register', adminRegister);  // TEMPORARY - Remove in production
 router.post('/student/register', studentRegister);
 router.post('/student/login', studentLogin);
 
